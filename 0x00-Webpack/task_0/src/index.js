128a11e1d0d2304ddf98ac182fdf4e102b3b867a
AxiomDays <tobeoke@gmail.com>
2024-11-01 11:23:16 +0100

    task0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+element = $("body")
+
+element.append("<p>Holberton Dashboard</p>")
+element.append("<p>Dashboard data for the students</p>")
+element.append("<p>Copyright - Holberton School</p>")
