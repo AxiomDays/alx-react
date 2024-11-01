bef4b526ae3cf82c92cc942b8485779a9acc3bff
AxiomDays <tobeoke@gmail.com>
2024-11-01 11:47:46 +0100

    task00012

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
 const $ = require("jquery");
 
-element = $("body")
+const element = $("body")
 
 element.append("<p>Holberton Dashboard</p>")
 element.append("<p>Dashboard data for the students</p>")
