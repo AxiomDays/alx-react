e0af6e4bf28f35b7ca416826d1c5d269a7e8ecea
AxiomDays <tobeoke@gmail.com>
2024-11-05 16:34:01 +0100

    first_push

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,12 @@
+function getFullYear(){
+	const d = new Date();
+	return d.getFullYear();
+}
+
+function getFooterCopy(isIndex){
+	if (isIndex){
+		return ("Holberton School")
+	} else {
+		return ("Holberton School main dashboard")
+	}
+}
